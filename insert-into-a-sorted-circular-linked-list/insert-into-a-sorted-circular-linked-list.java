3e21ec17cc9d9f78e920ff0cb2f4cb527bc72534
Rabid Desire <rabiddesire@users.noreply.github.com>
2021-12-02 15:34:54 -0800

    Time: 0 ms (100.00%), Space: 38.3 MB (82.28%) - LeetHub

---
+++ insert-into-a-sorted-circular-linked-list/insert-into-a-sorted-circular-linked-list.java
@@ -0,0 +1,36 @@
+class Solution {
+  public Node insert(Node head, int insertVal) {
+    if (head == null) {
+      Node newNode = new Node(insertVal, null);
+      newNode.next = newNode;
+      return newNode;
+    }
+
+    Node prev = head;
+    Node curr = head.next;
+    boolean toInsert = false;
+
+    do {
+      if (prev.val <= insertVal && insertVal <= curr.val) {
+        // Case 1).
+        toInsert = true;
+      } else if (prev.val > curr.val) {
+        // Case 2).
+        if (insertVal >= prev.val || insertVal <= curr.val)
+          toInsert = true;
+      }
+
+      if (toInsert) {
+        prev.next = new Node(insertVal, curr);
+        return head;
+      }
+
+      prev = curr;
+      curr = curr.next;
+    } while (prev != head);
+
+    // Case 3).
+    prev.next = new Node(insertVal, curr);
+    return head;
+  }
+}
