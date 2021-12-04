49bfef0bf728b15bce537d7bbfa6244c16c5e104
Rabid Desire <rabiddesire@users.noreply.github.com>
2021-12-04 00:09:55 -0800

    Time: 0 ms (100.00%), Space: 40.2 MB (62.86%) - LeetHub

---
+++ linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode slow = head; 
+        ListNode fast = head.next;
+        while(slow != null && fast != null && slow != fast) {   
+            slow = slow.next;
+            if (fast.next == null) return false;
+            fast = fast.next.next;
+        }
+        if (fast == slow) {
+                return true;
+        } else {
+                return false;
+        }
+    }
+}
