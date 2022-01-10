7878b570159f2ace77ed1cf5f6467b3dc573f2e6
Rabid Desire <rabiddesire@users.noreply.github.com>
2022-01-09 17:14:33 -0800

    Time: 1 ms (39.59%), Space: 40.4 MB (27.97%) - LeetHub

---
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode sentinel = new ListNode(0, head);
+        ListNode pred = sentinel;
+        
+        while (head != null) {
+            if (head.next != null && head.val == head.next.val) {
+                while (head.next != null && head.val == head.next.val) head = head.next;    
+                pred.next = head.next;
+            } else {
+                pred = pred.next;    
+            }
+            head = head.next;    
+        }
+        return sentinel.next;
+    }
+}
