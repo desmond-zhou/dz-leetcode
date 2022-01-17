d8bbf0d6f1d650bef0833776af765e523fcaa9d1
Rabid Desire <rabiddesire@users.noreply.github.com>
2022-01-17 12:50:49 -0800

    Time: 5 ms (27.91%), Space: 46.1 MB (23.26%) - LeetHub

---
+++ 573-squirrel-simulation/573-squirrel-simulation.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minDistance(int height, int width, int[] tree, int[] squirrel, int[][] nuts) {
+        int nutDistance = 0;
+        int maxSaving = Integer.MIN_VALUE;
+        for (int[] nut : nuts) {
+            int nutToTree = calculate(nut, tree);
+            nutDistance += nutToTree * 2;
+            int nutToSquirrel = calculate(nut, squirrel);
+            int saving = nutToTree - nutToSquirrel;
+            maxSaving = Math.max(saving, maxSaving);
+        }
+        return nutDistance - maxSaving;
+    }
+    
+    private int calculate(int[] obj1, int[] obj2) {
+        return Math.abs(obj1[0] - obj2[0]) + Math.abs(obj1[1] - obj2[1]);
+    }
+}
