28c8a46fa6465eece104ff7899bdce6f5eef1eb3
Rabid Desire <rabiddesire@users.noreply.github.com>
2022-02-02 01:20:59 -0800

    Time: 441 ms (48.73%), Space: 117.7 MB (47.24%) - LeetHub

--- 2060-check-if-an-original-string-exists-given-two-encoded-strings/2060-check-if-an-original-string-exists-given-two-encoded-strings.java
+++ 2060-check-if-an-original-string-exists-given-two-encoded-strings/2060-check-if-an-original-string-exists-given-two-encoded-strings.java
@@ -1,90 +1,105 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 class Solution {
 
+    /**
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        boolean result = sol.possiblyEquals("g141g9g94q592q62g78g779q61g779g28", "g2q4g795g87g828q321g532g195q11g77");
+        System.out.println(result);
+    } **/
+
     final int[] singleSkip = new int[]{0, 0};
+    final HashSet<List<Integer>> failMemo = new HashSet<>();
+    String string1;
+    String string2;
 
     public boolean possiblyEquals(String s1, String s2) {
-        return recurse(s1, 0, 0, s2, 0, 0);
+        this.string1 = s1;
+        this.string2 = s2;
+        return recurse( 0, 0, 0, 0);
     }
 
-    private Set<int[]> extractSkips(String s1, int start1) {
-        Set<int[]> result = new HashSet<>();
-        generateSkipsRecur(s1, start1, result, 0, 0);
-        return result;
-    }
-
-    private void generateSkipsRecur(String string, int digitStart, Set<int[]> result, int valueSoFar, int lengthSoFar) {
-        int end = digitStart;
-        if (end > string.length() - 1 || !Character.isDigit(string.charAt(end))) {
-            result.add(new int[]{lengthSoFar, valueSoFar});
-            return;
-        }
-        int curNum = 0;
-        while (end < string.length() && Character.isDigit(string.charAt(end))) {
-            curNum = curNum * 10 + Character.getNumericValue(string.charAt(end));
-            generateSkipsRecur(string, end + 1, result, valueSoFar + curNum, lengthSoFar + end - digitStart + 1);
-            end++;
-        }
-    }
-
-
-    HashSet<List<Integer>> failMemo = new HashSet<>();
-
-    private boolean recurse(String string1, int i1, int wildcards1, String string2, int i2, int wildCards2) {
+    private boolean recurse(int i1, int wildcards1, int i2, int wildCards2) {
         int commonReduce = Math.min(wildcards1, wildCards2);
         wildcards1 -= commonReduce;
         wildCards2 -= commonReduce;
 
         while (true) {
             if (wildcards1 > 0) {
-                if (i2 >= string2.length()) return false;
-                if (Character.isAlphabetic(string2.charAt(i2))) {
+                if (isEnd(i2, string2)) return false;
+                if (isLetter(i2, string2)) {
                     wildcards1--;
                     i2++;
-                } else break;
+                }
+                else break;
             } else if (wildCards2 > 0) {
-                if (i1 >= string1.length()) return false;
-                if (Character.isAlphabetic(string1.charAt(i1))) {
+                if (isEnd(i1, string1)) return false;
+                if (isLetter(i1, string1)) {
                     wildCards2--;
                     i1++;
-                } else break;
+                }
+                else break;
             } else {
-                if (i2 >= string2.length() && i1 >= string1.length()) return true;
-                else if (i2 >= string2.length()) return false;
-                else if (i1 >= string1.length()) return false;
-                else if (Character.isAlphabetic(string1.charAt(i1)) && Character.isAlphabetic(string2.charAt(i2))) {
-                    if (string1.charAt(i1) != string2.charAt(i2)) return false;
+                if (isEnd(i2, string2) && isEnd(i1, string1)) return true; // both done
+                else if (isEnd(i2, string2) || isEnd(i1, string1)) return false; // one end other did not
+                else if (isLetter(i1, string1) && isLetter(i2, string2)) { // both letter
+                    if (string1.charAt(i1) != string2.charAt(i2)) return false; // no match is fail
                     i1++;
                     i2++;
-                } else break;
+                }
+                else break;
             }
         }
 
-        final List<Integer> state = List.of(i1, wildcards1, i2, wildCards2);
+        // one of the wildcard and following letter is exhausted, the other wildcard still remains
 
-        if (failMemo.contains(state)) return false;
+        final List<Integer> memoState = List.of(i1, wildcards1, i2, wildCards2);
 
-        Set<int[]> skips1 = i1 < string1.length() && Character.isDigit(string1.charAt(i1)) ? extractSkips(string1, i1) : Set.of(singleSkip);
-        Set<int[]> skips2 = i2 < string2.length() && Character.isDigit(string2.charAt(i2)) ? extractSkips(string2, i2) : Set.of(singleSkip);
+        if (failMemo.contains(memoState)) return false;
+
+        Set<int[]> skips1 = !isEnd(i1, string1) && !isLetter(i1, string1) ? extractSkips(string1, i1) : Set.of(singleSkip);
+        Set<int[]> skips2 = !isEnd(i2, string2) && !isLetter(i2, string2) ? extractSkips(string2, i2) : Set.of(singleSkip);
 
         for (int[] candidates1 : skips1) {
             for (int[] candidates2 : skips2) {
-                int newI1 = i1 + candidates1[0];
-                int newI2 = i2 + candidates2[0];
-
-                int wild1 = wildcards1 + candidates1[1];
-                int wild2 = wildCards2 + candidates2[1];
-
-                boolean result = recurse(string1, newI1, wild1, string2, newI2, wild2);
+                boolean result = recurse(
+                        i1 + candidates1[0],
+                        wildcards1 + candidates1[1],
+                        i2 + candidates2[0],
+                        wildCards2 + candidates2[1]);
                 if (result) return true;
             }
         }
 
-        failMemo.add(state);
+        failMemo.add(memoState);
         return false;
+    }
+
+    private Set<int[]> extractSkips(String string, int start) {
+        Set<int[]> result = new HashSet<>();
+        generateSkipsRecur(string, start, result, 0, 0);
+        return result;
+    }
+
+    private void generateSkipsRecur(String string, int digitStart, Set<int[]> result, int valueSoFar, int lengthSoFar) {
+        int end = digitStart;
+        if (isEnd(end, string) || isLetter(end, string)) {
+            result.add(new int[]{lengthSoFar, valueSoFar});
+            return;
+        }
+        int curNum = 0;
+        while (!isEnd(end, string) && !isLetter(end, string)) {
+            curNum = curNum * 10 + Character.getNumericValue(string.charAt(end));
+            int segLength = end - digitStart + 1;
+            generateSkipsRecur(string, end + 1, result, valueSoFar + curNum, lengthSoFar + segLength);
+            end++;
+        }
+    }
+
+    private boolean isLetter(int index, String string) {
+        return Character.isAlphabetic(string.charAt(index));
+    }
+
+    private boolean isEnd(int index, String string) {
+        return index >= string.length();
     }
 }
