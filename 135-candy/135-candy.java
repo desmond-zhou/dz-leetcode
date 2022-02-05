082387278b2fe0c626162c8a697918fe5b8065cd
Rabid Desire <rabiddesire@users.noreply.github.com>
2022-02-04 20:27:48 -0800

    Time: 2 ms (96.41%), Space: 42.8 MB (57.99%) - LeetHub

---
+++ 135-candy/135-candy.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int[] left = new int[ratings.length];
+        left[0] = 1;
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i-1]) left[i] = left[i-1] + 1;
+            else left[i] = 1;
+        }
+        
+        int[] right = new int[ratings.length];
+        right[ratings.length - 1] = 1;
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i+1]) right[i] = right[i+1] + 1;
+            else right[i] = 1;
+        }
+        
+        int counter = 0;
+        for (int i = 0; i < ratings.length; i++) {
+            counter += Math.max(left[i], right[i]);
+        }
+        
+        return counter;
+    }
+}
