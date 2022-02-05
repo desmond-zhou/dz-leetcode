c7151fbf8e3a571e2a4e42a488d521bc42f1c37b
Rabid Desire <rabiddesire@users.noreply.github.com>
2022-02-04 19:18:27 -0800

    Time: 7 ms (53.19%), Space: 47.4 MB (8.81%) - LeetHub

---
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        final PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> Integer.compare(a.val, b.val));
+        for (ListNode n : lists) {
+            if (n != null) pq.add(n);
+        }
+        
+        ListNode newRoot = new ListNode(0);
+        ListNode cur = newRoot;
+        while (!pq.isEmpty()) {
+            ListNode node = pq.poll();
+            cur.next = node;
+            cur = node;
+            if (node.next != null) {
+                pq.offer(node.next);
+            }
+            node.next = null;
+        }
+        
+        return newRoot.next;
+    }
+}
