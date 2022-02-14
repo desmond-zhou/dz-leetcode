8efc075114b18012869271479d732df7e71a90f2
Rabid Desire <rabiddesire@users.noreply.github.com>
2022-02-13 22:45:38 -0800

    Time: 25 ms (5.92%), Space: 43.7 MB (5.45%) - LeetHub

---
+++ 274-h-index/274-h-index.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] papers = new int[n + 1];
+        // counting papers for each citation number
+        for (int c: citations)
+            papers[Math.min(n, c)]++;
+        // finding the h-index
+        int k = n;
+        for (int s = papers[n]; k > s; s += papers[k]) {
+            System.out.println("Sum of paper above: " + k + " ,is: " + s);
+            k--;
+        }
+        return k;
+    }
+}
