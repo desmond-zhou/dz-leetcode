f3e360158d572821b9d213a660ce1c9464e27281
Rabid Desire <rabiddesire@users.noreply.github.com>
2022-09-23 21:27:04 -0700

    Time: 59 ms (29.87%), Space: 13.7 MB (18.36%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.py
+++ 162-find-peak-element/162-find-peak-element.py
@@ -1,11 +1,11 @@
 class Solution(object):
-    def findPeakElement(self, nums):
-        l = 0
-        r = len(nums) - 1
-        while (l < r):
-            mid = (l + r) // 2
-            if nums[mid] > nums[mid + 1]:
-                r = mid
-            else:
-                l = mid + 1
-        return l
+        def findPeakElement(self, nums):
+            l = 0
+            r = len(nums) - 1
+            while (l < r):
+                mid = (l + r) // 2
+                if nums[mid] > nums[mid + 1]:
+                    r = mid
+                else:
+                    l = mid + 1
+            return l
